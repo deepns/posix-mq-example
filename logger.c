6767072ee08f44596647e2d135449c5b6346c3de
Deepan <57165211+deepns@users.noreply.github.com>
2020-03-28 09:03:49 -0400

    indent using spaces

--- logger.c
+++ logger.c
@@ -5,7 +5,7 @@
 #include "logger.h"
 
 const char *loglevel_strings[] = {
-	FOREACH_LOGLEVEL(ENUM_STR)
+    FOREACH_LOGLEVEL(ENUM_STR)
 };
 
 static loglevel_t log_level = DEFAULT_LOGLEVEL;
@@ -13,19 +13,19 @@ static loglevel_t log_level = DEFAULT_LOGLEVEL;
 static int
 log_level_is_valid(loglevel_t level)
 {
-	return (level >= log_level);
+    return (level >= log_level);
 }
 
 void
 set_log_level(loglevel_t level)
 {
-	log_level = level;
+    log_level = level;
 }
 
 const char *
 loglevel_to_str(loglevel_t level)
 {
-	return loglevel_strings[level];
+    return loglevel_strings[level];
 }
 
 /*
@@ -36,31 +36,31 @@ loglevel_to_str(loglevel_t level)
 void
 logger(loglevel_t level, const char *fmt, ...)
 {
-	va_list args;
-	char buffer[MAX_LOGMSG_LEN];
-	time_t cur_time;
-	char time_str[32];
+    va_list args;
+    char buffer[MAX_LOGMSG_LEN];
+    time_t cur_time;
+    char time_str[32];
 
-	if (!log_level_is_valid(level)) {
-		return;
-	}
+    if (!log_level_is_valid(level)) {
+        return;
+    }
 
-	/*
-	 * Stringify the arguments.
-	 */
-	va_start(args, fmt);
-	(void)vsnprintf(buffer, sizeof(buffer), fmt, args);
-	va_end(args);
+    /*
+     * Stringify the arguments.
+     */
+    va_start(args, fmt);
+    (void)vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
 
-	/*
-	 * Get the current time as string
-	 */
-	cur_time = time(NULL);
-	struct tm *local_time = localtime(&cur_time);
-	strftime(time_str, sizeof(time_str), "%c", local_time);
+    /*
+     * Get the current time as string
+     */
+    cur_time = time(NULL);
+    struct tm *local_time = localtime(&cur_time);
+    strftime(time_str, sizeof(time_str), "%c", local_time);
 
-	/*
-	 * Sending to stdout only for now.
-	 */
-	fprintf(stdout, "[%s],%s,%s\n", time_str, loglevel_to_str(level), buffer);
+    /*
+     * Sending to stdout only for now.
+     */
+    fprintf(stdout, "[%s],%s,%s\n", time_str, loglevel_to_str(level), buffer);
 }
